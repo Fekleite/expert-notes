8d6976ef3035d06f3b05008b32e5554c4d260a1f
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-02-07 10:24:33 -0300

    feat: add textarea to input notes

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,7 +1,20 @@
+import { ChangeEvent, useState } from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
 import { X } from 'lucide-react'
 
 export function NewNoteCard() {
+  const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
+
+  function handleStartEditor() {
+    setShouldShowOnboarding(false);
+  }
+
+  function handleContentChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    if (event.target.value === "") {
+      setShouldShowOnboarding(true)
+    }
+  }
+
   return (
     <Dialog.Root>
       <Dialog.Trigger className='rounded-md flex flex-col bg-slate-700 p-5 gap-3 text-left'>
@@ -20,9 +33,17 @@ export function NewNoteCard() {
           <div className='flex flex-1 flex-col gap-3 p-5'>
             <span className='text-small font-medium text-slate-200'>Adicionar nota</span>
 
-            <p className='text-small leading-6 text-slate-400'>
-              Comece <button className='font-medium text-lime-400 hover:underline'>gravando uma nota</button> em áudio ou se preferir <button className='font-medium text-lime-400 hover:underline'>utilize apenas texto</button>.
-            </p>
+            {shouldShowOnboarding ? (
+              <p className='text-small leading-6 text-slate-400'>
+                Comece <button className='font-medium text-lime-400 hover:underline'>gravando uma nota</button> em áudio ou se preferir <button className='font-medium text-lime-400 hover:underline' onClick={handleStartEditor}>utilize apenas texto</button>.
+              </p>
+            ) : (
+              <textarea
+                autoFocus
+                className='text-small leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none'
+                onChange={handleContentChange}
+              />
+            )}
           </div>
 
           <button type="button" className='w-full bg-lime-400 py-4 text-center text-small text-lime-950 outline-none font-medium hover:bg-lime-500'>
