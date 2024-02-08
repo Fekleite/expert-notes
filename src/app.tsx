b4b9c35e87436d1fccf82337fda7a8736404b00e
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-02-08 10:00:34 -0300

    feat: add methods to create notes

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,29 @@
+import { useState } from 'react'
+
 import logo from './assets/logo-nlw-expert.svg'
 
 import { NoteCard } from './components/note-card'
 import { NewNoteCard } from './components/new-note-card'
 
+interface Note {
+  id: string
+  date: Date
+  content: string
+}
+
 export function App() {
+  const [notes, setNotes] = useState<Note[]>([])
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content
+    }
+
+    setNotes(prevState => [newNote, ...prevState])
+  }
+
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
       <img src={logo} alt="NLW Expert" />
@@ -19,10 +39,11 @@ export function App() {
       <div className='h-px bg-slate-700' />
 
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
-        <NewNoteCard />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
 
-        <NoteCard note={{ date: new Date(), content: 'hello world' }} />
-        <NoteCard note={{ date: new Date(), content: 'hello here' }} />
+        {notes.map(note => (
+          <NoteCard key={note.id} note={note} />
+        ))}
       </div>
     </div>
   )
